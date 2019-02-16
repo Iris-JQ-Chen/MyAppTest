6ef12c9bc9305ba865682642b84678e972837072
Iris JQ Chen <2906514421@qq.com>
2019-02-16 11:48:20 +0800

    CreateDialogFragment

--- app/src/main/java/com/example/myapptest/MainActivity.java
+++ app/src/main/java/com/example/myapptest/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.myapptest;
 
+import android.support.annotation.IdRes;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FireMissilesDialogFragment dialogFragment = new FireMissilesDialogFragment();
+                FragmentManager manager = getSupportFragmentManager();
+                dialogFragment.show(manager,"dealogFrag1");
+            }
+        });
     }
 }
