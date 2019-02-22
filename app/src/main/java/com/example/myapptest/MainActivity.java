ddd2673e232204d5d1879667ae038f19b193db0f
Iris JQ Chen <2906514421@qq.com>
2019-02-22 13:52:39 +0800

    使用Calendar抽象类找到当前时间

--- app/src/main/java/com/example/myapptest/MainActivity.java
+++ app/src/main/java/com/example/myapptest/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.myapptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MyAppTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        Integer integer = null;
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH)+1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        StringBuilder builder = new StringBuilder(year);
+        builder.append(month);
+        builder.append(day);
+        builder.append(hour);
+        builder.append(minute);
+        builder.append(second);
+        integer = new Integer(builder.toString());
+
+        Log.d(TAG,year+"");
+        Log.d(TAG,month+"");
+        Log.d(TAG,day+"");
+        Log.d(TAG,hour+"");
+        Log.d(TAG,minute+"");
+        Log.d(TAG,second+"");
+        Log.d(TAG,integer.toString());
+
+//        Toast.makeText(this,integer+"",Toast.LENGTH_SHORT).show();
     }
 }
