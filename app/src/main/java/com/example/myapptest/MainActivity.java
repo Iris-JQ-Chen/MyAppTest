78b699b264c74971361351c913bbd7b71807d706
Iris JQ Chen <2906514421@qq.com>
2019-02-27 11:53:33 +0800

    matisse使用完成

--- app/src/main/java/com/example/myapptest/MainActivity.java
+++ app/src/main/java/com/example/myapptest/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.myapptest;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.zhihu.matisse.Matisse;
+import com.zhihu.matisse.MimeType;
+import com.zhihu.matisse.engine.impl.GlideEngine;
+import com.zhihu.matisse.internal.entity.CaptureStrategy;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_CODE_CHOOSE = 23;
+    Button button;
+    TextView textView;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textview);
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Matisse.from(MainActivity.this)
+                        .choose(MimeType.allOf())//图片类型
+                        .countable(true)//true:选中后显示数字;false:选中后显示对号
+                        .maxSelectable(5)//可选的最大数
+                        .capture(true)//选择照片时，是否显示拍照
+                        .captureStrategy(new CaptureStrategy(true, "com.example.xx.fileprovider"))//参数1 true表示拍照存储在共有目录，false表示存储在私有目录；参数2与 AndroidManifest中authorities值相同，用于适配7.0系统 必须设置
+                        .imageEngine(new GlideEngine())//图片加载引擎
+                        .forResult(REQUEST_CODE_CHOOSE);//
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_CHOOSE && resultCode == RESULT_OK) {
+            List<Uri> result = Matisse.obtainResult(data);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(getImagePath(result.get(0),null)));
+            textView.setText(result.toString());
+        }
+    }
+
+    private String getImagePath(Uri uri,String selection){
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        if(cursor != null){
+            if(cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
     }
 }
