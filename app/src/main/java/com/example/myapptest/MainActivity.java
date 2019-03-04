3ae74792627956aa05f59e7a106816ae73d84cc5
Iris JQ Chen <2906514421@qq.com>
2019-03-04 17:05:57 +0800

    第一次网络版本，不理想（上一次commit出错不算）

--- app/src/main/java/com/example/myapptest/MainActivity.java
+++ app/src/main/java/com/example/myapptest/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.myapptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.myapptest.adapter.MainRecyclerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mainRecyclerVeiw = (RecyclerView)findViewById(R.id.main_recycler_view);
+        MainRecyclerAdapter adapter = new MainRecyclerAdapter();
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        mainRecyclerVeiw.setAdapter(adapter);
+        mainRecyclerVeiw.setLayoutManager(layoutManager);
     }
 }
--- app/src/main/java/com/example/myapptest/adapter/GridAdapter.java
+++ app/src/main/java/com/example/myapptest/adapter/GridAdapter.java
@@ -1,8 +1,40 @@
 package com.example.myapptest.adapter;
 
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapptest.R;
+
 /**
  * Created by 蒲公英之流 on 2019-03-04.
  */
 
-public class GridAdapter {
+public class GridAdapter extends RecyclerView.Adapter<GridAdapter.ViewHolder> {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view_grid, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText("Grid "+position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 8;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final AppCompatTextView mTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView= (AppCompatTextView) itemView.findViewById(R.id.textView);
+        }
+    }
 }
