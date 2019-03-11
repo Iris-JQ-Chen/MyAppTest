a17ceb3d28883edb3ba7079af74b8701ecca6157
Iris JQ Chen <2906514421@qq.com>
2019-03-11 17:59:39 +0800

    打开音频文件成功

--- app/src/main/java/com/example/myapptest/MainActivity.java
+++ app/src/main/java/com/example/myapptest/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.myapptest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int OPEN_ALBUM = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("audio/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent,OPEN_ALBUM);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case OPEN_ALBUM:
+                if (resultCode == RESULT_OK){
+                    ((TextView)findViewById(R.id.text1)).setText(data.getData().toString());
+                }
+                return;
+        }
     }
 }
