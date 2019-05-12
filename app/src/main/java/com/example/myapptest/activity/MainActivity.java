cc5c59c70cf8625aeddb2091a007337d4acee7f7
Iris JQ Chen <2906514421@qq.com>
2019-05-12 11:47:56 +0800

    与后台tomcat&servlet链接实现登录注册 使用okhttp和asynctask——完成

--- app/src/main/java/com/example/myapptest/activity/MainActivity.java
+++ app/src/main/java/com/example/myapptest/activity/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myapptest.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,16 +26,23 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     public static final int SHOW_RESPONSE=1;
     public Handler handler=new Handler() {
-        public void handleMessage(Message msg)
-        {
+        public void handleMessage(Message msg) {
             switch (msg.what){
                 case SHOW_RESPONSE:
+                    Log.d("MyAppTest","test7");
                     String response=(String)msg.obj;
                     Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
                     break;
@@ -55,9 +64,9 @@ public class MainActivity extends AppCompatActivity {
                 EditText pwd=(EditText)findViewById(R.id.password);
                 String id=uid.getText().toString().trim();
                 String pw=pwd.getText().toString().trim();
-                Toast.makeText(MainActivity.this,"send前",Toast.LENGTH_SHORT).show();
-                SendByHttpClient(id,pw);
-                Toast.makeText(MainActivity.this,"send后",Toast.LENGTH_SHORT).show();
+                MainActivity.MyTestAsyncTask task =new MainActivity.MyTestAsyncTask();
+                task.execute(id,pw);
+//                SendByHttpClient(id,pw);
             }
         });
     }
@@ -67,25 +76,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    HttpClient httpclient=new DefaultHttpClient();
-                    HttpPost httpPost=new HttpPost("http://192.168.13.1:8888/untitled3_war_exploded/Login");
-                    List<NameValuePair> params=new ArrayList<NameValuePair>();
-                    params.add(new BasicNameValuePair("ID",id));
-                    params.add(new BasicNameValuePair("PW",pw));
-                    final UrlEncodedFormEntity entity=new UrlEncodedFormEntity(params,"utf-8");
-                    httpPost.setEntity(entity);
+//                    HttpClient httpclient=new DefaultHttpClient();
+//                    HttpPost httpPost=new HttpPost("http://192.168.13.1:8888/untitled3_war_exploded/Login");
+//                    List<NameValuePair> params=new ArrayList<NameValuePair>();
+//                    params.add(new BasicNameValuePair("ID",id));
+//                    params.add(new BasicNameValuePair("PW",pw));
+//                    final UrlEncodedFormEntity entity=new UrlEncodedFormEntity(params,"utf-8");
+//                    httpPost.setEntity(entity);
                     Toast.makeText(MainActivity.this,"已发送",Toast.LENGTH_SHORT).show();
-                    HttpResponse httpResponse= httpclient.execute(httpPost);
+                    Log.d("MyAppTest","已发送");
+//                    HttpResponse httpResponse= httpclient.execute(httpPost);
                     Toast.makeText(MainActivity.this,"已接收",Toast.LENGTH_SHORT).show();
-                    if(httpResponse.getStatusLine().getStatusCode()==200)
-                    {
-                        HttpEntity entity1=httpResponse.getEntity();
-                        String response=EntityUtils.toString(entity1, "utf-8");
-                        Message message=new Message();
-                        message.what=SHOW_RESPONSE;
-                        message.obj=response;
-                        handler.sendMessage(message);
-                    }
+                    Log.d("MyAppTest","未发送");
+//                    if(httpResponse.getStatusLine().getStatusCode()==200)
+//                    {
+//                        HttpEntity entity1=httpResponse.getEntity();
+//                        String response=EntityUtils.toString(entity1, "utf-8");
+//                        Message message=new Message();
+//                        message.what=SHOW_RESPONSE;
+//                        message.obj=response;
+//                        handler.sendMessage(message);
+//                    }
                 }
                 catch (Exception e) {
                     e.printStackTrace();
@@ -114,5 +125,37 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class MyTestAsyncTask extends AsyncTask<String,Void,Void>{
+        @Override
+        protected Void doInBackground(String... param) {
+            String id = param[0];
+            String pw = param[1];
+
+//          使用okhttp
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("ID",id)
+                    .add("PW",pw)
+                    .build();
+            Request request = new Request.Builder()
+                    .url("http://10.6.194.113:8080/Login")
+                    .post(requestBody)
+                    .build();
+            try {
+                Response response = okHttpClient.newCall(request).execute();
+                String s = response.body().string();
+                Log.d("MyAppTest",s);
+                Message message = new Message();
+                message.what = SHOW_RESPONSE;
+                message.obj = s;
+                handler.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
