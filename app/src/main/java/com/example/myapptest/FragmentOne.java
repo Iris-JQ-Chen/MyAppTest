57fe7e38785dd9484493282f19e655a88ecb9c96
Iris JQ Chen <2906514421@qq.com>
2019-06-19 15:44:05 +0800

    viewpager+fragment的滑动尝试，成功

---
+++ app/src/main/java/com/example/myapptest/FragmentOne.java
@@ -0,0 +1,22 @@
+package com.example.myapptest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 蒲公英之流 on 2019-06-18.
+ */
+
+public class FragmentOne extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+}
